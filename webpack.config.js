1b24835bdcf2d9992f0ed62192c06fcd5d6f324c
Dmitriy Klimenko <33551076+hitmanet@users.noreply.github.com>
2020-11-03 17:06:25 +0300

    VEGA-504: Настроить сборку общих зависимостей в vega-shell (#18)
    * feat(shell): добавил shared dependencies
    
    Jira issue: VEGA-504
    
    * feat(dependencies): добавление общих зависимостей
    
    Jira issue: VEGA-504
    
    * fix(dependencies): фикс зависимостей single-spa
    
    Jira issue: VEGA-504
    
    * feat(webpack): добавление зависимостей
    
    Jira issue: VEGA-504
    
    * fix(deps): исправить сборку общих зависимостей
    
    Jira issue: VEGA-504
    
    * feat(dependencies): добавление systemjs
    
    Jira issue: VEGA-504
    
    * fix(import-map): фикс импорт-мапы
    
    Jira issue: VEGA-504
    
    * fix(webpack): фикс параметра devtool
    
    Jira issue: VEGA-504
    
    * feat(webpack): поправил конфиги
    
    Jira issue: VEGA-504
    
    * fix(webpack): фикс билда
    
    Jira issue: VEGA-504
    
    * fix(webpack): изменил прокидывание env, добавил rimraf
    
    Jira issue: VEGA-504
    
    * fix(webpack): фикс получения env
    
    Jira issue: VEGA-504
    
    * fix(webpack): фикс формирования импорт мапы
    
    Jira issue: VEGA-504
    
    Co-authored-by: Sergey Gavshin <inzephirum@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ const singleSpaDefaults = require('webpack-config-single-spa-ts');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkerPlugin = require('worker-plugin');
 const ImportMapPlugin = require('webpack-import-map-plugin');
-
-const PORT = process.env.PORT || 3000;
-const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { join } = require('path');
 
 function withTrailingSlash(path) {
   if (path.endsWith('/')) {
@@ -15,7 +14,58 @@ function withTrailingSlash(path) {
   return `${path}/`;
 }
 
-module.exports = (webpackConfigEnv) => {
+const systemDependencies = {
+  development: {
+    'systemjs': 'systemjs/dist/system.js',
+    'systemjs-amd': 'systemjs/dist/extras/amd.js',
+    'systemjs-named-exports': 'systemjs/dist/extras/named-exports.js',
+    'import-map-overrides': 'import-map-overrides/dist/import-map-overrides.js',
+  },
+  production: {
+    'systemjs': 'systemjs/dist/system.min.js',
+    'systemjs-amd': 'systemjs/dist/extras/amd.min.js',
+    'systemjs-named-exports': 'systemjs/dist/extras/named-exports.min.js',
+    'import-map-overrides': 'import-map-overrides/dist/import-map-overrides.js',
+  },
+};
+
+const sharedDependencies = {
+  development: {
+    'react': 'react/cjs/react.development.js',
+    'react-dom': 'react-dom/cjs/react-dom.development.js',
+    'single-spa': 'single-spa/lib/umd/single-spa.dev.js',
+    'graphql': 'graphql/index.js',
+    '@apollo/client': '@apollo/client/index.js',
+  },
+  production: {
+    'react': 'react/cjs/react.production.min.js',
+    'react-dom': 'react-dom/cjs/react-dom.production.min.js',
+    'single-spa': 'single-spa/lib/umd/single-spa.min.js',
+    'graphql': 'graphql/index.js',
+    '@apollo/client': '@apollo/client/apollo-client.cjs.min.js',
+  },
+};
+
+function getPort(webpackConfigEnv) {
+  let port = process.env.PORT || 3000;
+
+  if (webpackConfigEnv !== undefined && 'port' in webpackConfigEnv) {
+    port = webpackConfigEnv.port;
+  }
+
+  return port;
+}
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const singleSpaConfig = (webpackConfigEnv) => {
+  const PORT = getPort(webpackConfigEnv);
+  const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
+
+  const importNamesList = Object.keys(sharedDependencies[NODE_ENV])
+    .map((key) => `${key}.js`)
+    .filter((key) => !Object.keys(systemDependencies[NODE_ENV]).includes(key));
+
   const orgName = 'vega';
   const defaultConfig = singleSpaDefaults({
     orgName,
@@ -23,14 +73,27 @@ module.exports = (webpackConfigEnv) => {
     webpackConfigEnv,
   });
 
-  return webpackMerge.smart(defaultConfig, {
+  defaultConfig.plugins = defaultConfig.plugins.filter((plugin) => {
+    return !(plugin instanceof CleanWebpackPlugin);
+  });
+
+  const config = webpackMerge.smart(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    name: 'single-spa',
     devServer: {
       historyApiFallback: true,
       proxy: {
         '/graphql': 'http://outsourcing.nat.tepkom.ru:38300/',
       },
     },
+    entry: {
+      'vega-shell': defaultConfig.entry,
+      ...sharedDependencies[NODE_ENV],
+    },
+    output: {
+      filename: '[name].js',
+    },
+    externals: Object.keys(sharedDependencies[NODE_ENV]),
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
@@ -46,11 +109,21 @@ module.exports = (webpackConfigEnv) => {
         fileName: 'import-map.json',
         baseUrl: withTrailingSlash(BASE_URL),
         filter(x) {
-          return ['main.js'].includes(x.name);
+          return ['vega-shell.js', ...importNamesList].includes(x.name);
         },
         transformKeys(filename) {
-          if (filename === 'main.js') {
+          if (filename === 'vega-shell.js') {
             return '@vega/shell';
+          }
+
+          if (filename === 'apollo-client.js') {
+            return '@apollo/client';
+          }
+
+          const extIndex = filename.indexOf('.js');
+
+          if (extIndex > -1) {
+            return filename.slice(0, extIndex);
           }
 
           return undefined;
@@ -58,4 +131,21 @@ module.exports = (webpackConfigEnv) => {
       }),
     ],
   });
+
+  return config;
 };
+
+const systemConfig = {
+  name: 'system-js',
+  entry: {
+    ...systemDependencies[NODE_ENV],
+  },
+  devtool: 'sourcemap',
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    path: join(__dirname, 'dist'),
+  },
+};
+
+module.exports = [singleSpaConfig, systemConfig];
