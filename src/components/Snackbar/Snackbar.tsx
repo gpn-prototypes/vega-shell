1619e46f9ae3649c2c4d0df34e37e7c11ba88387
Gurinovich Roman <31928264+fixmylie@users.noreply.github.com>
2020-11-30 12:44:21 +0300

    VEGA-520: Создать центр уведомлений в веге (#46)
    * feat(global): добавить нотификации
    
    Jira issue: VEGA-520
    
    * feat(global): прокинуть notifications в vega-auth
    
    Jira issue: VEGA-520

---
+++ src/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Item } from '@consta/uikit/SnackBar';
+import { SnackBar as BaseSnackbar, usePortalRender } from '@gpn-prototypes/vega-ui';
+import singleSpaReact from 'single-spa-react';
+
+import { Notifications } from '../../utils/notifications';
+
+import './Snackbar.css';
+
+const styles = {
+  container: 'vega-shell-snackbar-container',
+  snackbar: 'vega-shell-snackbar',
+};
+
+type SnackBarProps = {
+  notifications: Notifications;
+};
+
+const Snackbar: React.FC<SnackBarProps> = (props) => {
+  const [notifications, setNotifications] = React.useState<Item[]>([]);
+  const { renderPortalWithTheme } = usePortalRender();
+
+  React.useEffect(() => {
+    const unsubscribe = props.notifications.subscribe('change', ({ items }) => {
+      setNotifications(items);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [props.notifications]);
+
+  return renderPortalWithTheme(
+    <div className={styles.container}>
+      <BaseSnackbar items={notifications} className={styles.snackbar} />
+    </div>,
+    document.body,
+  );
+};
+
+export const SnackbarLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: Snackbar,
+  suppressComponentDidCatchWarning: true,
+});
