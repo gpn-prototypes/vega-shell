aca7335ae711691d1260eff34aa021e4c92cc4d4
Dmitriy Klimenko <33551076+hitmanet@users.noreply.github.com>
2020-12-07 16:23:11 +0300

    fix(env): брать хосты из env (#58)
    Jira issue: VEGA-719

--- app-config/index.js
+++ app-config/index.js
@@ -8,7 +8,7 @@ const getAppConfig = () => ({
   port: process.env.PORT || 3000,
   mode: process.env.NODE_ENV || 'development',
   analyze: process.env.ANALYZE || 0,
-  baseApiUrl: process.env.BASE_API_URL || 'http://outsourcing.nat.tepkom.ru:38080',
+  baseApiUrl: process.env.BASE_API_URL,
   apiPath: process.env.API_PATH || '/',
   useApiProxy: process.env.USE_API_PROXY || false,
 });
