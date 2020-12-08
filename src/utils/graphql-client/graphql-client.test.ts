cdf1ebc7c3ac9f9fe0e085730cbbb6271aa8c8ab
Oleg <c1n1kk@gmail.com>
2020-12-08 11:07:08 +0300

    VEGA-636: Добавить поддержку эндпоинта для проекта (#35)
    * feat(apollo): добавить link для управления endpoint клиента
    
    * fix(Link): переименовать Link, поправить тесты
    
    * refactor(test): удалить лишнюю функцию
    
    * fix: добавить номрализацию uri, сделат тесты асинхронными
    
    Jira issue: VEGA-636
    
    Co-authored-by: Sergey Gavshin <inzephirum@gmail.com>

--- src/utils/graphql-client/graphql-client.test.ts
+++ src/utils/graphql-client/graphql-client.test.ts
@@ -1,10 +1,41 @@
-import { ApolloLink, execute, Observable, throwServerError, toPromise } from '@apollo/client';
+import {
+  ApolloLink,
+  execute,
+  FetchResult,
+  Observable,
+  throwServerError,
+  toPromise,
+} from '@apollo/client';
+import fetchMock from 'fetch-mock';
 
-import { createErrorLink, createResponseLink } from './graphql-client';
+import { Identity } from '../identity';
+
+import {
+  createAuthLink,
+  createErrorLink,
+  createHttpLink,
+  createResponseLink,
+  createSwitchUriLink,
+  normalizeUri,
+} from './graphql-client';
 import { mocks, queries } from './mocks';
 
 beforeEach(() => {
   jest.restoreAllMocks();
+});
+
+describe('normalizeUri', () => {
+  test('удалает последний слэш', () => {
+    expect(normalizeUri('/test/')).toBe('/test');
+  });
+
+  test('удаляет повторения слэшей', () => {
+    expect(normalizeUri('//test///foo/bar///')).toBe('/test/foo/bar');
+  });
+
+  test('учитывает протокол', () => {
+    expect(normalizeUri('/http:///foo.bar//graphql')).toBe('http://foo.bar/graphql');
+  });
 });
 
 describe('responseLink', () => {
@@ -47,5 +78,69 @@ describe('errorLink', () => {
     } catch {
       expect(handleError).toBeCalledWith({ code: 500, message: 'internal-server-error' });
     }
+  });
+});
+
+function makePromise(res: unknown) {
+  return new Promise((resolve) => setTimeout(() => resolve(res)));
+}
+
+describe('client', () => {
+  const data: FetchResult = { data: { hello: 'world' } };
+
+  beforeEach(() => {
+    fetchMock.restore();
+  });
+
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('меняется uri', async () => {
+    fetchMock.post('/graphql/projectVid1', makePromise(data));
+
+    const URI = 'graphql';
+    const PROJECT_VID = 'projectVid1';
+    const CONFIG = { apiUrl: '/auth', token: 'test-token' };
+
+    const uriLink = createSwitchUriLink(URI);
+
+    const identity = new Identity(CONFIG);
+    const authLink = createAuthLink(identity);
+
+    const link = ApolloLink.from([authLink, uriLink, createHttpLink()]);
+
+    await toPromise(
+      execute(link, {
+        query: queries.sample,
+        context: {
+          projectVid: PROJECT_VID,
+        },
+      }),
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const [uri] = fetchMock.lastCall()!;
+    expect(uri).toBe('/graphql/projectVid1');
+  });
+
+  it('не меняется uri', async () => {
+    fetchMock.post('/graphql', makePromise(data));
+
+    const URI = 'graphql';
+    const CONFIG = { apiUrl: '/auth', token: 'test-token' };
+
+    const uriLink = createSwitchUriLink(URI);
+
+    const identity = new Identity(CONFIG);
+    const authLink = createAuthLink(identity);
+
+    const link = ApolloLink.from([authLink, uriLink, createHttpLink()]);
+
+    await toPromise(execute(link, { query: queries.sample }));
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const [uri] = fetchMock.lastCall()!;
+    expect(uri).toBe('/graphql');
   });
 });
