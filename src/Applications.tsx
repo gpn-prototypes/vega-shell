20d0432f96d7a54c7ba78cf0179a70940f2877d4
Dmitriy Klimenko <33551076+hitmanet@users.noreply.github.com>
2020-12-21 12:15:45 +0300

    VEGA-780: [В МАСТЕР] Не отображать хедер на страницах 404 и 500 (#72)
    * fix(server-error): поправил обработку ошибки с сервера
    
    Jira issue: VEGA-780
    
    * chore(types): фикс типов для билда
    
    Jira issue: VEGA-780

--- src/Applications.tsx
+++ src/Applications.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { Root } from '@gpn-prototypes/vega-ui';
 
+import { ServerError } from './services/graphql-client';
 import { AppContext, AppContextProps } from './app-context';
 import { ApplicationRoutes, Snackbar } from './components';
 
 import './App.css';
 
-type Props = AppContextProps;
+type Props = Omit<AppContextProps, 'serverError' | 'setServerError'>;
 
 export const Applications = (props: Props): React.ReactElement => {
+  const [serverError, setServerError] = React.useState<ServerError | null>(null);
+
   return (
-    <AppContext.Provider value={props}>
+    <AppContext.Provider value={{ ...props, serverError, setServerError }}>
       <Root defaultTheme="dark" className="AppParcel">
         <Router history={props.history}>
           <ApplicationRoutes />
