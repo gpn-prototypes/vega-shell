6b8d832304e928d9b40777112c81ebee9c44c37c
Sergey Gavshin <inzephirum@gmail.com>
2020-12-26 16:50:28 +0700

    feat(bus): добавить возможность не шарить событие

--- src/services/message-bus/worker.ts
+++ src/services/message-bus/worker.ts
@@ -3,6 +3,12 @@ import type { WorkerInputMessageEvent, WorkerOutput } from './types';
 // eslint-disable-next-line no-restricted-globals
 const worker = (self as unknown) as SharedWorkerGlobalScope;
 
+interface NotifyParams {
+  initiator: MessagePort;
+  self: boolean;
+  broadcast: boolean;
+}
+
 class Hub {
   private ports: Set<MessagePort>;
 
@@ -22,9 +28,26 @@ class Hub {
     }
   }
 
-  public notify(output: WorkerOutput, options: { exclude?: MessagePort } = {}) {
+  public notify(output: WorkerOutput, params: NotifyParams) {
     this.ports.forEach((port) => {
-      if (port !== options.exclude) {
+      if (params.self && port === params.initiator) {
+        port.postMessage(output);
+        return;
+      }
+
+      if (!params.broadcast) {
+        return;
+      }
+
+      if (output.type === 'message') {
+        port.postMessage({
+          ...output,
+          meta: {
+            ...output.meta,
+            self: false,
+          },
+        });
+      } else {
         port.postMessage(output);
       }
     });
@@ -51,12 +74,13 @@ worker.addEventListener('connect', (e) => {
       };
 
       const params = {
-        self: inputMessage.self ?? false,
+        self: inputMessage.self ?? true,
+        broadcast: inputMessage.broadcast ?? false,
       };
 
       hub.notify(
         { type: 'message', payload: message, meta: params },
-        { exclude: params.self === false ? port : undefined },
+        { initiator: port, self: params.self, broadcast: params.broadcast },
       );
 
       return;
