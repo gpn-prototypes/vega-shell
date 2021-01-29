fa7755b87e37ebcca92d2d38dfa2692821a22feb
Sergey Gavshin <inzephirum@gmail.com>
2021-01-29 21:10:40 +0700

    chore(jest): убрать мок localStorage
    Jira issue: VEGA-865

--- setup-tests.ts
+++ setup-tests.ts
@@ -2,10 +2,4 @@ import fetch from 'cross-fetch';
 
 import '@testing-library/jest-dom';
 
-import { MockLocalStorage } from './src/testing/MockLocalStorage';
-
-beforeEach(() => {
-  Object.defineProperty(global.window, 'localStorage', { value: new MockLocalStorage() });
-});
-
 global.fetch = fetch;
