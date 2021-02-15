b996011f5724fff0f7435f4cba44ea76405610d3
hitmanet <nathan200033@gmail.com>
2021-02-15 13:27:26 +0300

    fix(project-page): поправил переход со страницы создания проекта
    Jira issue: VEGA-753

--- src/components/ApplicationRoutes/ApplicationRoutes.tsx
+++ src/components/ApplicationRoutes/ApplicationRoutes.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
-import { useMount, useOnChange, usePreviousRef } from '@gpn-prototypes/vega-ui';
+import { Loader, useMount, useOnChange, usePreviousRef } from '@gpn-prototypes/vega-ui';
 
 import { useShell } from '../../app';
 import { ServerError } from '../../services/graphql-client';
@@ -26,6 +26,18 @@ const NotFoundView = () => (
 export const AUTH_ERROR_MESSAGE =
   'Что-то пошло не так. Для повторного входа в\u00A0систему введите свои e-mail и\u00A0пароль';
 
+const LogoutView = (): React.ReactElement | null => {
+  const { identity } = useShell();
+
+  useMount(() => {
+    if (identity.isLoggedIn()) {
+      identity.logout();
+    }
+  });
+
+  return <Loader />;
+};
+
 export const ApplicationRoutes = (): React.ReactElement => {
   const shell = useShell();
   const { serverError, setServerError } = shell;
@@ -33,7 +45,9 @@ export const ApplicationRoutes = (): React.ReactElement => {
 
   const location = useLocation();
 
-  const [isLoggedIn, setIsLoggedIn] = useState(identity.isLoggedIn());
+  const [_, setIsLoggedIn] = useState(identity.isLoggedIn());
+
+  const isLoggedIn = identity.isLoggedIn();
 
   useMount(() => {
     const authUnsub = bus.subscribe<{ loggedIn: boolean }>(
@@ -70,7 +84,6 @@ export const ApplicationRoutes = (): React.ReactElement => {
 
   const previousPathname = usePreviousRef(location.pathname).current;
 
-  // Оставил в обвязке, так как она управляет роутингом и нотификациями и тестами будет проще покрывать
   useOnChange(location.pathname, () => {
     if (previousPathname !== null && previousPathname !== location.pathname) {
       // istanbul ignore else
@@ -78,62 +91,74 @@ export const ApplicationRoutes = (): React.ReactElement => {
         setServerError(null);
       }
     }
-    // istanbul ignore else
-    if (identity.isLoggedIn() !== isLoggedIn) {
-      setIsLoggedIn(identity.isLoggedIn());
-    }
   });
-
-  const getLoginPath = (): string => {
-    return `${AUTH_PATH}?redirectTo=${encodeURIComponent(location.pathname)}`;
-  };
 
   const getLoginRedirectPath = (): string => {
     const query = new URLSearchParams(location.search);
     return query.get('redirectTo') ?? '/projects';
   };
 
+  const getLoginPath = (): string => {
+    const basePath = AUTH_PATH;
+    if (
+      previousPathname === AUTH_PATH ||
+      previousPathname === '/logout' ||
+      previousPathname === null
+    ) {
+      return basePath;
+    }
+
+    return `${basePath}?redirectTo=${encodeURIComponent(previousPathname)}`;
+  };
+
+  if (!isLoggedIn && location.pathname !== AUTH_PATH) {
+    return <Redirect to={getLoginPath()} />;
+  }
+
+  if (serverError !== null) {
+    return <ErrorView {...serverError} />;
+  }
+
   return (
-    <>
-      {serverError && <ErrorView {...serverError} />}
-      {!isLoggedIn && location.pathname !== AUTH_PATH && <Redirect to={getLoginPath()} />}
-      <Switch>
-        {isLoggedIn && <Redirect from="/" to="/projects" exact />}
-        <Route exact path={AUTH_PATH}>
-          {isLoggedIn && <Redirect to={getLoginRedirectPath()} />}
-          <AuthPage />
-        </Route>
-        <Route path="/projects">
-          <ApolloProvider client={graphqlClient}>
-            <Header />
-          </ApolloProvider>
-          <main className="main">
-            <Switch>
-              <Route exact path={['/projects/show/:projectId/rb']}>
-                <Application name="@vega/rb" />
-              </Route>
-              <Route exact path={['/projects/show/:projectId/lc']}>
-                <Application name="@vega/lc" />
-              </Route>
-              <Route
-                exact
-                path={[
-                  '/projects/show/:projectId/fem',
-                  '/projects/show/:projectId/fem/OPEX',
-                  '/projects/show/:projectId/fem/CAPEX',
-                ]}
-              >
-                <Application name="@vega/fem" />
-              </Route>
-              <Route exact path={['/projects', '/projects/create', '/projects/show/:projectId']}>
-                <Application name="@vega/sp" />
-              </Route>
-              <NotFoundView />
-            </Switch>
-          </main>
-        </Route>
-        <NotFoundView />
-      </Switch>
-    </>
+    <Switch>
+      {isLoggedIn && <Redirect exact from="/" to="/projects" />}
+      <Route path={AUTH_PATH}>
+        {isLoggedIn && <Redirect to={getLoginRedirectPath()} />}
+        <AuthPage />
+      </Route>
+      <Route path="/logout">
+        <LogoutView />
+      </Route>
+      <Route path="/projects">
+        <ApolloProvider client={graphqlClient}>
+          <Header />
+        </ApolloProvider>
+        <main className="main">
+          <Switch>
+            <Route exact path={['/projects/show/:projectId/rb']}>
+              <Application name="@vega/rb" />
+            </Route>
+            <Route exact path={['/projects/show/:projectId/lc']}>
+              <Application name="@vega/lc" />
+            </Route>
+            <Route
+              exact
+              path={[
+                '/projects/show/:projectId/fem',
+                '/projects/show/:projectId/fem/OPEX',
+                '/projects/show/:projectId/fem/CAPEX',
+              ]}
+            >
+              <Application name="@vega/fem" />
+            </Route>
+            <Route exact path={['/projects', '/projects/create', '/projects/show/:projectId']}>
+              <Application name="@vega/sp" />
+            </Route>
+            <NotFoundView />
+          </Switch>
+        </main>
+      </Route>
+      <NotFoundView />
+    </Switch>
   );
 };
