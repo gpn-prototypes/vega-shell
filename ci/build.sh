ce65a23d59de739e3a40140814bced3c3e5ebbb9
Maksim Kononov <maksim.kononov.csssr@gmail.com>
2021-02-18 12:09:31 +0300

    chore(global): добавил дополнительные переменные
    Jira issue: VEGA-915

--- ci/build.sh
+++ ci/build.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+if [ -z "$VEGA_REPOPREFIX" ]
+then
+  VEGA_REPOPREFIX="vega-shell"
+fi
+
+if [ -z "$IMAGE_NAME" ]
+then
+  IMAGE_NAME="frontend-builder"
+fi
+
 if [ -z "$FE_BUILDER_VERSION" ]
 then
   FE_BUILDER_VERSION="1"
 fi
 
-IS_IMAGE_EXIST=$(docker image ls | grep -w "vega/frontend-builder")
+TAG="$VEGA_REPOPREFIX/$IMAGE_NAME:$FE_BUILDER_VERSION"
+
+IS_IMAGE_EXIST=$(docker image ls | grep -w "$VEGA_REPOPREFIX/$IMAGE_NAME")
 
 if [ -n "$IS_IMAGE_EXIST" ]
 then
   echo "Docker image already exist. Skip build command."
 else
   echo "Docker image do not exist. Run build command."
-  docker build -t vega/frontend-builder:$FE_BUILDER_VERSION ./ci
+  docker build -t $TAG ./ci
 fi
 
 docker run \
@@ -20,5 +32,5 @@ docker run \
   -v "$(pwd):/app" \
   --env NPM_URI=$NPM_URI \
   --env NPM_AUTH_TOKEN=$NPM_AUTH_TOKEN \
-  vega/frontend-builder:$VERSION \
+  $TAG \
   /app/ci/build-entrypoint.sh
