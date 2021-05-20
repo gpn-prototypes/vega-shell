bfc53a0dd71f8984304e367350c0c0724bba1a27
Leonid Tumoyakov <leonid.tumoyakov@gmail.com>
2021-05-20 19:27:32 +0700

    [VEGA-3846] fix(NotFoundView): Исправление сообщения для 404 ошибки
    Jira issue: VEGA-3846

--- src/components/ApplicationRoutes/NotFoundView.tsx
+++ src/components/ApplicationRoutes/NotFoundView.tsx
@@ -3,9 +3,5 @@ import React from 'react';
 import { ErrorView } from '../Error';
 
 export const NotFoundView: React.FC = () => (
-  <ErrorView
-    code={404}
-    message="page-not-found"
-    userMessage="Ошибка 404. Страница не найдена. Обратитесь в службу технической поддержки"
-  />
+  <ErrorView code={404} message="page-not-found" userMessage="Указанная страница не найдена" />
 );
