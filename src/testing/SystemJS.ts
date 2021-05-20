846ea201e2f76375f96a45dbc1a914e2a693e3e1
Garics2000 <emtzev@gmail.com>
2021-05-20 18:40:30 +0300

    Removed none YC env targeted template statements; Added some linters instructions; (#205)

--- src/testing/SystemJS.ts
+++ src/testing/SystemJS.ts
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+/* eslint-disable */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import singleSpaReact, { Lifecycles } from 'single-spa-react';
